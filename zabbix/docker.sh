53268c058a7167f45b8673e946e820bd7144367d
Ubuntu <azureuser@zabbix.2d3v514oxq4uzeeufapaywcnse.bx.internal.cloudapp.net>
2024-06-10 18:22:30 +0000

    Zabbix study

---
+++ zabbix/docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install ca-certificates curl
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+sudo apt install -y docker-ce
+
+sudo usermod -aG docker azureuser
+
+echo "Instalação completa"
+
+echo "Instalando Docker Compose"
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
